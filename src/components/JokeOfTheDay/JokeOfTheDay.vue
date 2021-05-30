9c319abb8816b8d34a6b8e492c9c6631b3f534f4
Badar <bapk65@gmail.com>
2021-05-30 17:51:59 +0500

    refactor: remove unused code

--- src/components/JokeOfTheDay/JokeOfTheDay.vue
+++ src/components/JokeOfTheDay/JokeOfTheDay.vue
@@ -6,25 +6,24 @@
 import Banner from "@/components/Banner/Banner";
 
 export default {
-  name: 'JokeOfTheDay',
+  name: "JokeOfTheDay",
   components: {
     Banner,
   },
   data() {
     return {
       api: "https://api.jokes.one/jod?category=knock-knock",
-      jokeOfTheDay: { joke: { text: '' } }
-    }
+      jokeOfTheDay: { joke: { text: "" } },
+    };
   },
   mounted() {
-    fetch(this.api, { headers: { 'Content-Type': "application/json" } }).then((response) => {
-      response.json().then(result => {
-        this.jokeOfTheDay = result.contents.jokes[0];
-      })
-    })
-  }
-}
+    fetch(this.api, { headers: { "Content-Type": "application/json" } }).then(
+      (response) => {
+        response.json().then((result) => {
+          this.jokeOfTheDay = result.contents.jokes[0];
+        });
+      }
+    );
+  },
+};
 </script>
-
-<style>
-</style>
